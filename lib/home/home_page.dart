70fac441f1611920a8d21c21cf495862d7487641
Wagner.Araujo <araujowp@gmail>
2021-04-22 19:04:29 -0300

    segundo dia

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:devquiz/home/widgets/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +13,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true, 
+                children: [
+                  LevelButtonWidget(label: 'Easy'),
+                  SizedBox(width: 10),
+                  LevelButtonWidget(label: 'Middle'),
+                  SizedBox(width: 10),
+                  LevelButtonWidget(label: 'Hard'),
+                  SizedBox(width: 10),
+                  LevelButtonWidget(label: 'Expert'),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing:16,
+                crossAxisCount: 2,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
